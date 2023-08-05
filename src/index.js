d07d12afe0c2327aaf298e1ec511f29d7083c144
alvar91 <aleksey91scorp@bk.ru>
2023-08-05 12:05:05 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const bracketsPair = bracketsConfig.map((item) => item.join(""));
+
+  let i = 0;
+  while (i < bracketsPair.length) {
+    if (str.includes(bracketsPair[i])) {
+      str = str.replace(bracketsPair[i], "");
+      i = 0;
+    } else i++;
+  }
+
+  return str.length ? false : true;
+};
